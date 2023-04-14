3ebd0d0b059b193aba29225f8673ee46e75a526b
Viktor Moskalenko <v.moskalenko@brightlab.me>
2023-04-14 19:15:47 +0300

    added rule for git reset --soft

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+/** @format */
+
 /**
- * main branch
+ * * git reset
+ * ? Три этапа:
+ * * 1.changes in working dir
+ * * 2. add files to working dir (git add .)
+ * * 3. commit changes (git commit)
+ */
+
+/**
+ *! git reset --soft [hash commit (на который хочешь откатиться)] - эта команда откатывает 3-й этап (т.е. коммит)
  *
- * @format
  */
